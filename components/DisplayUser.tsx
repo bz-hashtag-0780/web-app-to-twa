1d38ae138adf0d1fbefffff6d4ba93160bbc3d82
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2024-12-28 02:51:37 +0100

    Update DisplayUser.tsx

--- components/DisplayUser.tsx
+++ components/DisplayUser.tsx
@@ -24,7 +24,7 @@ export default function DisplayUser() {
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen text-center space-y-4">
 			<h1 className="text-4xl font-bold">
-				Welcome to the Telegram Web App!
+				Welcome to the Telegram Web App! 2
 			</h1>
 			<p>User ID: {userID || 'Not available'}</p>
 			<p>Username: {username || 'Not available'}</p>
